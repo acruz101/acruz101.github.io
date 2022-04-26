ec8c66a9e3c963017c9bdf30ce0fd5077c309f06
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-04-25 17:56:14 -0700

    draft 2 - new projects pages

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,38 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var img2 = document.getElementById("myImg2");
+var modalImg = document.getElementById("img01");
+var modalImg2 = document.getElementById("img02");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close1");
+var span2 = document.getElementById("close2");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() { 
+    modal2.style.display = "none";
+  }
